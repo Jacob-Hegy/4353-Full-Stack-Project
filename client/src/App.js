bfca616fe3f4f1592d64da965fdc9c3ac283898c
MalikTaylor <67035475+MalikTaylor@users.noreply.github.com>
2023-02-20 19:11:36 -0600

    Added local npm directory, Sign up page and Login page

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,17 @@ import {
 import RootLayout from "./layouts/RootLayout";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="contactus" element={<ContactUs />} />
+      {/* Need to implement Login detetction */}
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
     </Route>
   )
 );
