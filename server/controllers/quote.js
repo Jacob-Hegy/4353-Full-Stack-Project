6524d88ecd5e35b4327d5d93523699d50e227e91
Jacob-Hegy <JacobHegy@gmail.com>
2023-03-23 18:23:57 -0500

    Fixed the SQL queries, waiting on the frontend

--- server/controllers/quote.js
+++ server/controllers/quote.js
@@ -1,16 +1,16 @@
 export const addQuote = async (req, res) => {
     try {
         const { id } = req.params;
-        const { address, city, state, zip, phone, productType, gals} = req.body;
-        db.query("SELECT * FROM user WHERE id = ?", [id], (err, data) => {
+        const { address, city, state, zip, phone, gals} = req.body;
+        db.query("SELECT * FROM UserCredentials WHERE id = ?", [id], (err, data) => {
           if (err) return res.status(500).json(err);
 
           // user found
           let price = gals * 3.98;
           if (data.length) {
             db.query(
-              "INSERT INTO fuelquote (userID, productType, gals, price, street_address, phone, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-              [id, productType, gals, price, address, phone, city, state, zip],
+              "INSERT INTO FuelQuotes (ID, GallonsRequested, SuggestedPrice, DeliveryAddress, DeliveryDate, Total) VALUES (?, ?, ?, ?, ?, ?)", // DeliveryDate is currently set as data type "Date"; check functionality
+              [id, gals, price, address, phone, city, state, zip],
               (err, data) => {
                 if (err) return res.status(500).json(err);
                 return res.status(200).json(data[0]);
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,7 +13,7 @@ export const getUser = async (req, res) => {
           res.status(403).json(null);
         }
 
-        db.query("SELECT * from client WHERE userID=?", [userId.id])
+        db.query("SELECT * from ClientInformation WHERE ID = ?", [userId.id])
           .then((data) => res.status(200).json(data[0][0]))
           .catch((err) => res.status(500).json(err));
       });
@@ -29,13 +29,14 @@ export const saveProfile = async (req, res) => {
   try {
     const { id } = req.params;
     const { fullname, address1, address2, city, state, zip } = req.body;
+    const stateList = {"Alabama": "AL", "Alaska": "AK", "American Samoa": "AS", "Arizona": "AZ", "Arkansas": "AR", "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE", "District Of Columbia": "DC", "Federated States Of Micronesia": "FM", "Florida": "FL", "Georgia": "GA", "Guam": "GU", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Marshall Islands": "MH", "Maryland": "MD", "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV", "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC", "North Dakota": "ND", "Northern Mariana Islands": "MP", "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Palau": "PW", "Pennsylvania": "PA", "Puerto Rico": "PR", "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virgin Islands": "VI", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV", "Wisconsin": "WI", "Wyoming": "WY"};
     db.query("SELECT * FROM UserCredentials WHERE ID = ?", [id], (err, data) => {
       if (err) return res.status(500).json(err);
       // user found
       if (data.length) {
         db.query(
           "UPDATE ClientInformation SET Name = ?, Address1 = ?, Address2 = ?, City = ?, State = ?, ZipCode = ? WHERE ID = ?",
-          [fullname, address1, address2, city, state, zip, id],
+          [fullname, address1, address2, city, stateList[state], zip, id],
           (err, data) => {
             if (err) return res.status(500).json(err);
             return res.status(200).json(data[0]);
